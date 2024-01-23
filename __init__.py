7674f292fbfb4f00979fc41d63fa7b074add0792
Alex Hallenbeck <alexhallenbeck1@gmail.com>
2024-01-22 23:57:27 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import cmd
+import json
 import os
 
 tracker_project = None
@@ -7,18 +8,41 @@ class ProjectCLI(cmd.Cmd):
     prompt = ":> "
     intro = "\nType 'help' for available project commands.\n"
 
-    def do_create(self, args):
-        """file path(str) - Creates a project JSON file at the entered directory."""
+    def do_create(self, line):
+        """file path(str), project name(str) - Creates a project JSON file at the entered directory."""
+
+        # Parse the arguments into a string and a float value.
+        path, name = [str(s) for s in line.split()]
+        name = f"{name}.json"
+        path = os.path.join(path, name)
+        data = {}
+
+        # Store the JSON data in a file
+        with open(path, "w") as file:
+            json.dump(data, file)
+
+        # Write the project name and directory to a json stored in the user documents directory.
 
     def do_set(self, args):
         """file path(str) - Sets the active project."""
 
+    def do_list(self, line):
+        """Lists all available projects."""
+        # Read our user json if it exists and list all the available projects.
+        return True
+
     def do_entry(self, line):
         """category(str), value(float) - Adds a data point to a category."""
+
+        # Parse the arguments into a string and a float value.
         category, value = [str(s) for s in line.split()]
         float(value)
-        print(f"{category}")
-        print(f"{value}")
+
+        # Check to see if the category exists already.
+
+        # Create the category if it doesn't exits.
+
+        # Add our data entry to the category in the json.
 
     def do_return(self, line):
         """Returns to the main Tracker CLI."""
